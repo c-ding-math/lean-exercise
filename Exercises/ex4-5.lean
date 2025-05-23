d7b297e6af0e8b122b2d7e4f5a78a774b898fd7a
Ding <c_ding@foxmail.com>
2025-05-23 14:44:58 +0800

    exist

--- Exercises/ex4-5.lean
+++ Exercises/ex4-5.lean
@@ -27,9 +27,34 @@ example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := Iff.intro
   (fun h : ¬ (∀ x, ¬ p x) => byContradiction fun h1 : ¬ (∃ x, p x) =>
     h (fun hx hpx => h1 ⟨hx, hpx⟩))
 
-example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := sorry
-example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := sorry
+example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := Iff.intro
+  (fun h x hpx => h ⟨x, hpx⟩)
+  (fun h => fun ⟨hx, hp⟩ => h hx hp)
+example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := Iff.intro
+  (fun h => byContradiction fun h1 : ¬ ∃ x, ¬ p x => h (fun x => Or.elim (Classical.em (p x))
+    (fun hp => hp)
+    (fun hnp => False.elim (h1 ⟨x, hnp⟩))))
+  (fun ⟨x, hnp⟩ h => hnp (h x))
+example : (∀ x, p x → r) ↔ (∃ x, p x) → r := Iff.intro
+  (fun h ⟨x, hp⟩ => h x hp)
+  (fun h x hp => h ⟨x, hp⟩)
 
-example : (∀ x, p x → r) ↔ (∃ x, p x) → r := sorry
-example (a : α) : (∃ x, p x → r) ↔ (∀ x, p x) → r := sorry
-example (a : α) : (∃ x, r → p x) ↔ (r → ∃ x, p x) := sorry
+example (a : α) : (∃ x, p x → r) ↔ (∀ x, p x) → r := Iff.intro
+  (fun ⟨x, hp⟩ h => hp (h x))
+  (fun h => Or.elim (Classical.em (∀ x, p x))
+    (fun y => ⟨a, λ _ =>h y⟩)
+    (fun hnp =>
+      have lemmap: ∃ x, ¬ p x := byContradiction
+        fun h1 : ¬ ∃ x, ¬ p x => hnp (fun x => Or.elim (Classical.em (p x))
+          (fun hpx => hpx)
+          (fun hnpx => False.elim (h1 ⟨x, hnpx⟩)))
+      let ⟨x0, hnp0⟩ := lemmap
+      ⟨x0, fun hp => False.elim (hnp0 hp)⟩))
+
+example (a : α) : (∃ x, r → p x) ↔ (r → ∃ x, p x) := Iff.intro
+  (fun ⟨x, hr⟩ h => ⟨x, hr h⟩)
+  (fun h => Or.elim (Classical.em r)
+    (fun hr =>
+      match h hr with
+      | ⟨x, hp⟩ => ⟨x, λ _ ↦ hp⟩)
+    (fun hnr => ⟨a, λ hr ↦ absurd hr hnr⟩))
